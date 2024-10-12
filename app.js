73b5da0fd4f78805ccd881f21de141bfc3b35bba
Akash Kumar Singh <90348968+webakash1806@users.noreply.github.com>
2024-10-12 12:58:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173","https://ebi.webakash1806.com"],
     credentials: true
 }))
 
